72e36e0bb0e78fef34c728a8de6ec6409c83dbb8
Nathan White <nw@nwhite.net>
2010-11-11 11:42:30 -0800

    initial example of middleware

--- examples/middleware.js
+++ examples/middleware.js
@@ -4,3 +4,49 @@
  *  - A parallel flow control (by next()ing immediately and fireing the done() callback
  *  when the task is completed
  */
+
+ var mongoose = require('../lib/mongoose')
+   , document = mongoose.define;
+
+ var db = mongoose.connect('mongodb://localhost/mongoose');
+
+ document('User')
+   .oid('_id')
+   .object('name',
+     document()
+       .string('first')
+       .string('last'))
+   .object('contact',
+     document()
+       .string('email')
+       .string('phone'))
+   .number('age')
+   .bool('blocked')
+ 
+   .hook('custom', function(fn){
+     console.log('custom hook');
+     if(fn) fn();
+   })
+   .hook('custom', function(parent,callback){ // arguments are the same as 'hook' except parent has been prepended.
+     console.log('overriding custom hook');
+     parent(callback); // 
+     //callback(); // if you want to skip default hook action.
+   })
+   .pre('custom', function(next,done){
+     console.log('pre hook for custom');
+     next();
+   })
+   .pre('custom', function(next, done){
+     console.log('another pre hook for custom');
+     next();
+   });
+
+
+ var User = mongoose.User;
+
+ var tobi = new User({ name: { first: 'Tobi', last: 'ferret' }, age: 1 });
+ 
+    tobi.custom(function(){
+      console.log('callback');
+      mongoose.disconnect();
+    });
