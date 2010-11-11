55b98af67aa4b86e3e68aca872b53451a7a08e18
bnoguchi <brian.noguchi@gmail.com>
2010-11-11 05:03:22 -0800

    Changed DBRef code to use native driver's DBRef constructor. Got all dbref, dbrefArray, and dbreffedArray types passing. Fixed conceptual bug I had introduced into util.walkObject.

---
+++ tests/dbref.test.js
@@ -0,0 +1,179 @@
+var assert = require('assert')
+  , mongoose = require('mongoose').new()
+  , db = mongoose.connect('mongodb://localhost/dbrefTests')
+  , DBRef = require('../support/node-mongodb-native/lib/mongodb/bson/bson').DBRef;
+
+mongoose.define('Dog')
+  .oid('_id')
+  .string('name');
+var Dog = mongoose.Dog;
+
+mongoose.define('User')
+  .oid('_id')
+  .string('name')
+  .dbref('dog', Dog)
+  .dbrefArray('puppies', Dog);
+var User = mongoose.User;
+
+// TODO api for refreshing/reloading data
+module.exports = {
+  setup: function (done) {
+    Dog.remove({}, function () {
+      User.remove({}, function () {
+        done();
+      });
+    });
+  },
+
+  'setting via JSON input should allow you to access a Document instance': function (assert, done) {
+    var user = new User({
+      name: 'Charlie',
+      dog: {name: 'Snoopy'}
+    });
+    user.dog.do( function (dog) {
+      assert.ok((dog instanceof Dog) === true);
+      done();
+    });
+  },
+
+  'setting to a new instance should allow you to access that new instance': function (assert, done) {
+    var user = new User({
+      name: 'Charlie',
+      dog: new Dog({name: 'Snoopy'})
+    });
+    user.dog.do( function (dog) {
+      assert.ok((dog instanceof Dog) === true);
+      assert.ok(dog.name === 'Snoopy');
+      done();
+    });
+  },
+
+  'setting to a persisted instance should automatically set the dbref params': function (assert, done) {
+    new Dog({
+      name: 'Pluto'
+    }).save( function (error, dog) {
+      var user = new User({
+        name: 'Mickey',
+        dog: dog
+      });
+      assert.ok(typeof dog._.doc._id !== "undefined");
+      assert.deepEqual(user._.doc.dog, new DBRef(dog._schema._collection, dog._.doc._id));
+      done();
+    });
+  },
+
+  'saving after setting the dbref to unpersisted data should automatically save that data to its own document': function (assert, done) {
+    new User({
+      name: 'Timmy',
+      dog: {
+        name: 'Lassie'
+      }
+    }).save( function (errors, user) {
+      Dog.find({name: 'Lassie'}).first( function (err, dog) {
+        user.dog.do( function (puppy) {
+          assert.ok(puppy.id === dog.id);
+          done();
+        });
+      });
+    });
+  },
+
+  'over-riding a dbref target with a different one should change the dbref params AND the de-referenced dbref': function (assert, done) {
+    var user = new User({
+      name: 'Charlie',
+      dog: {
+        name: 'Snoopy'
+      }
+    });
+    user.dog.do( function (dog1) {
+      var id1 = user._.doc.dog['$id'];
+      user.dog = new Dog({name: 'Woodstock'});
+      user.dog.do( function (dog2) {
+        var id2 = user._.doc.dog.oid;
+        assert.ok(dog1.id !== dog2.id);
+        assert.ok(id1 !== id2);
+        done();
+      });
+    });
+  },
+
+  'saving the referer after setting to a new dbref should update the dbref params': function (assert, done) {
+    new User({
+      name: 'Ed',
+      dog: {
+        name: 'Marmaduke'
+      }
+    }).save( function (errors, user) {
+      var id1 = user._.doc.dog.oid
+      user.dog = new Dog({ name: 'Droopy' });
+      user.save( function (errors, user) {
+        var id2 = user._.doc.dog['$id'];
+        assert.ok(id1 !== id2);
+        done();
+      });
+    });
+  },
+
+  'updating the dbref params directly should change the retrieved target on the next get': function (assert, done) {
+    var user = new User({
+      name: 'Lynn',
+      dog: { // dog1
+        name: 'Squeak'
+      }
+    });
+    new Dog({ // dog2
+      name: 'Banana Smashie'
+    }).save( function (errors, dog2) {
+      user.dog.do( function (dog1) { // dog1 should be Squeak
+        user._.doc.dog.oid = dog2._.doc._id;
+        user.dog.do( function (refreshedDog) {
+          assert.ok(refreshedDog.name === dog2.name);
+          done();
+        });
+      });
+    });
+  },
+
+//  'saving the source after setting to a persisted instance should not update the de-referenced target': function () {
+//  },
+
+  'setting properties of the dbref target should persist when we save the referring (source) instance': function (assert, done) {
+    new User({
+      name: 'Turner',
+      dog: {name: 'Hooch'}
+    }).save( function (errors, user) {
+      user.dog.do( function (dog) {
+        dog.name = 'Hooch the 2nd';
+        user.save( function (errors, user) {
+          Dog.findById(dog.id, function (err, found) {
+            assert.ok(found.name === 'Hooch the 2nd');
+            done();
+          });
+        });
+      })
+    });
+  },
+
+  'test removing the dbref target': function (assert, done) {
+    new User({
+      name: 'Shaggy',
+      dog: {
+        name: 'Scooby'
+      }
+    }).save( function (errors, user) {
+      user.dog.remove( function () {
+        assert.ok( typeof user._.doc.dog === "undefined");
+        user.dog.do( function (_dog) {
+          assert.ok( typeof _dog === "undefined" );
+          done();
+        });
+      });
+    });
+  },
+
+//  'initializing a referring user without a dbref should set it to the default dbref': function () {
+//  }
+  teardown: function(){
+    db.close();
+  }
+};
---
+++ tests/dbrefArray.test.js
@@ -0,0 +1,227 @@
+var assert = require('assert')
+  , mongoose = require('mongoose').new()
+  , db = mongoose.connect('mongodb://localhost/dbrefArrays')
+  , DBRef = require('../support/node-mongodb-native/lib/mongodb/bson/bson').DBRef;
+
+mongoose.define('Restaurant')
+  .oid('_id')
+  .string('name');
+var Restaurant = mongoose.Restaurant;
+
+mongoose.define('Cuisine')
+  .oid('_id')
+  .string('name')
+  .dbrefArray('restaurants', Restaurant);
+var Cuisine = mongoose.Cuisine;
+
+// TODO Show only send dirty members
+// TODO Test chaining
+module.exports = {
+  setup: function (done) {
+    Restaurant.remove({}, function () {
+      Cuisine.remove({}, function () {
+        done();
+      });
+    });
+  },
+
+  'setting via JSON input should allow you to access an array of Document instances': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Thai',
+      restaurants: [
+        { name: 'Osha Thai' },
+        { name: 'Thai Stick' }
+      ]
+    });
+    var count = 0;
+    cuisine.restaurants.forEach( function (diner) {
+      assert.ok(diner instanceof Restaurant);
+      if (++count === 2) done();
+    });
+  },
+
+  'setting to an array of new Document instances should allow you to access an array of Document instances AND set the dbref params': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Thai',
+      restaurants: [
+        new Restaurant({name: 'Osha Thai'}),
+        new Restaurant({name: 'Thai Stick'})
+      ]
+    });
+    var count = 0;
+    cuisine.restaurants.forEach( function (diner) {
+      assert.ok(diner instanceof Restaurant);
+      assert.ok(typeof diner._.doc._id !== "undefined");
+      assert.deepEqual(cuisine._.doc.restaurants[count], new DBRef(diner._schema._collection, diner._.doc._id));
+      if (++count === 2) done();
+    });
+  },
+
+  'saving after setting the dbref array to unpersisted data should automatically save that data to the db': function (assert, done) {
+    var count = 0;
+    new Cuisine({
+      name: 'Burgers',
+      restaurants: [
+        { name: 'Burger Bar' },
+        { name: "Pearl's Burgers" }
+      ]
+    }).save( function (errors, cuisine) {
+      cuisine.restaurants.forEach( function (diner) {
+        Restaurant.find({name: diner.name}).first( function (diner) {
+          return function (err, found) {
+            assert.ok(diner.id === found.id);
+            if (++count === 2) done();
+          };
+        }(diner));
+      });
+    });
+  },
+
+  'updating the dbref params directly should change the retrieved target on the next get': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Chinese',
+      restaurants: [
+        { name: 'PF Changs' }
+      ]
+    });
+    new Restaurant({ name: 'Panda Express'}).save( function (errors, diner2) {
+      cuisine.restaurants.forEach( function (diner1) {
+        cuisine._.doc.restaurants[0].oid = diner2._.doc._id;
+        cuisine.restaurants.forEach( function (refreshedDiner) {
+          assert.ok(refreshedDiner.name === diner2.name);
+          done();
+        });
+      });
+    });
+  },
+  
+  'setting properties of the dbref targets should persist when we save the referring (source) instance': function (assert, done) {
+    new Cuisine({
+      name: 'Japanese',
+      restaurants: [ { name: 'Katana Ya' } ]
+    }).save( function (errors, cuisine) {
+      cuisine.restaurants.forEach( function (diner) {
+        diner.name = 'Kiss Sushi';
+        cuisine.save( function (errors, cuis) {
+          Restaurant.findById(diner.id, function (err, found) {
+            assert.ok(found.name === 'Kiss Sushi');
+            done();
+          });
+        });
+      });
+    });
+  },
+
+  'pushing onto the dbref array should update the data we can retrieve': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'} ]
+    });
+    cuisine.restaurants.push({ name: "Dottie's" }).all( function (diners) {
+      assert.ok(diners.length === 3);
+      assert.ok(diners[2].name === "Dottie's");
+      done();
+    });
+  },
+
+  'test getting a specific member of the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'} ]
+    });
+    cuisine.restaurants.at(1, function (restaurant) {
+      assert.ok(restaurant.name === 'Farm:Table');
+      done();
+    });
+  },
+
+  'test getting a slice of the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'}, { name: "Dottie's"} ]
+    });
+    cuisine.restaurants.slice(1, 3, function (restaurants) {
+      assert.ok(restaurants.length === 2);
+      assert.ok(restaurants[0].name === 'Farm:Table');
+      assert.ok(restaurants[1].name === "Dottie's");
+      done();
+    });
+  },
+
+  'test filtering the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'}, { name: "Dottie's"} ]
+    });
+    cuisine.restaurants.filter( function (diner) {
+      return /a/.test(diner.name);
+    }).all( function (diners) {
+      assert.ok(diners.length === 2);
+      assert.ok(diners[0].name === 'Stacks');
+      assert.ok(diners[1].name === 'Farm:Table');
+      done();
+    });
+  },
+
+  'test mapping the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'}, { name: "Dottie's"} ]
+    });
+    cuisine.restaurants.map( function (diner) {
+      diner.name = diner.name + " - SF";
+      return diner;
+    }).all( function (diners) {
+      assert.ok(diners.length === 3);
+      assert.ok(diners[0].name === 'Stacks - SF');
+      assert.ok(diners[1].name === 'Farm:Table - SF');
+      assert.ok(diners[2].name === "Dottie's - SF");
+      done();
+    });
+  },
+
+  'test setting a specific member of the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' } ]
+    });
+    cuisine.restaurants[0] = new Restaurant({name: "Dottie's True Blue Cafe"});
+    cuisine.restaurants.forEach( function (diner) {
+      assert.ok(diner.name, "Dottie's True Blue Cafe");
+      done();
+    });
+  },
+
+//  'test splicing the dbref array': function (assert, done) {
+//    var cuisine = new Cuisine({
+//      name: 'Breakfast',
+//      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'}, { name: "Dottie's"} ]
+//    });
+//    cuisine.restaurants.splice(1, 2, new Restaurant({name: 'Hobees'}), new Restaurant({name: 'Moulin Cafe'}));
+//    cuisine.restaurants.map( function (diner) {
+//      return diner.name;
+//    }).all( function (dinerNames) {
+//      assert.ok(dinerNames[0] === 'Stacks');
+//      assert.ok(dinerNames[1] === 'Hobees');
+//      assert.ok(dinerNames[2] === "Dottie's");
+//      done();
+//    });
+//  },
+
+  // TODO Add in destroyDependent flag
+  'test clearing the dbref array': function (assert, done) {
+    var cuisine = new Cuisine({
+      name: 'Breakfast',
+      restaurants: [ { name: 'Stacks' }, {name: 'Farm:Table'}, { name: "Dottie's"} ]
+    });
+    cuisine.restaurants.clear();
+    cuisine.restaurants.all( function (diners) {
+      assert.ok(diners.length === 0);
+      done();
+    });
+  },
+
+  teardown: function(){
+    db.close();
+  }
+};
