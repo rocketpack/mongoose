b66649f687f50760d5a3b398e729cab3a97a15cd
Nathan White <nw@nwhite.net>
2010-11-15 03:55:19 -0800

    partial-hydration example added

--- examples/partial-hydration.js
+++ examples/partial-hydration.js
@@ -24,38 +24,94 @@
        .string('email')
        .string('phone'))
    .number('age')
-   .bool('blocked');
+   .bool('dead');
    
    
   var User = mongoose.User;
   
   var user = new User({
     name: { 
-      first: 'Nathan',
-      last: 'White'
+      first: 'Herman',
+      last: 'Melville'
     },
     contact: {
-      email: 'nathan@learnboost.com',
+      email: 'herman@melville.org',
       phone: '555-555-5555'
     },
-    age: 33
+    age: 91,
+    dead: true
   });
   
   user.save(function(err){
     
-    User.find({ 'name.last': 'White' }, { contact: true }).one(function(err, doc){
-      
-      console.log("Name: " + JSON.stringify(doc.name));
+    User.find({ 'name.last': 'Melville' }, { contact: true }).one(function(err, doc){
+//      console.log(doc.contact.phone)
+      console.log('-------- contact only --------');
+      console.log("Name: ", doc.name);
       console.log("Age: " + doc.age);
-      console.log("Contact: " + JSON.stringify(doc.contact));
+      console.log("Contact: ", doc.contact);
       console.log('contact hydrated: ', doc.hydrated('contact'));
       console.log('contact.phone hydrated: ', doc.hydrated('contact.phone'));
       console.log('name hydrated: ', doc.hydrated('name'));
+      console.log('age hydrated: ', doc.hydrated('age'));
       
-      User.drop(function(){
-        db.close();
+      console.log('-------- age only --------');
+      User.find({ dead: true }, 'age').one(function(err, doc){
+        console.log("Name: " + JSON.stringify(doc.name));
+        console.log("Age: " + doc.age);
+        console.log("Contact: " + JSON.stringify(doc.contact));
+        console.log('contact hydrated: ', doc.hydrated('contact'));
+        console.log('contact.phone hydrated: ', doc.hydrated('contact.phone'));
+        console.log('name hydrated: ', doc.hydrated('name'));
+        console.log('age hydrated: ', doc.hydrated('age'));
+      
+      
+        console.log('-------- not contact --------');
+        User.find({}, {contact: false}).one(function(err, doc){
+          console.log("Name: " + doc.name);
+          console.log("Age: " + doc.age);
+          console.log("Contact: " + JSON.stringify(doc.contact));
+          console.log('contact hydrated: ', doc.hydrated('contact'));
+          console.log('contact.phone hydrated: ', doc.hydrated('contact.phone'));
+          console.log('name hydrated: ', doc.hydrated('name'));
+          console.log('age hydrated: ', doc.hydrated('age'));
+          
+          User.drop(function(){
+            db.close();
+          });
+          
+        });
       });
-      
     });
-    
   });
+  
+/*
+
+  Expected Results:
+  
+  -------- contact only --------
+  Name:  undefined
+  Age: undefined
+  Contact:  [object Object]
+  contact hydrated:  true
+  contact.phone hydrated:  true
+  name hydrated:  false
+  age hydrated:  false
+  -------- age only --------
+  Name: undefined
+  Age: 91
+  Contact: undefined
+  contact hydrated:  false
+  contact.phone hydrated:  false
+  name hydrated:  false
+  age hydrated:  true
+  -------- not contact --------
+  Name: undefined
+  Age: 91
+  Contact: undefined
+  contact hydrated:  false
+  contact.phone hydrated:  false
+  name hydrated:  true
+  age hydrated:  true
+  
+*/
