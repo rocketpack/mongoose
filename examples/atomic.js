4d0166b1500bc4314ada2e2788529e81e4f65f9b
Nathan White <nw@nwhite.net>
2010-11-16 11:21:57 -0800

    added example for atomic operations

--- examples/atomic.js
+++ examples/atomic.js
@@ -7,3 +7,44 @@
  *     - The length of the array is 100
  *     - The order of the items is the expected one
  */
+
+ var assert = require('assert')
+   , mongoose = require('../lib/mongoose').new()
+   , document = mongoose.define
+   , db = mongoose.connect('mongodb://localhost/mongoose_test');
+
+ document('Atomic')
+   .oid('_id')
+   .string('name')
+   .array('items').atomic(true);
+   
+   
+  var Atomic = mongoose.Atomic
+    , atomic = new Atomic({ name: 'atomic', items: [] })
+    , loop = 100;
+  
+  for(var i = 0, l = loop; i < l; i++){
+    atomic.items.push(i);
+    atomic.save(function(err, doc){
+      if(err) console.log('this should not happen!');
+      if(--loop === 0){
+        // And were done
+        // lets check the results
+        Atomic.find('name', 'atomic').one(function(err, doc){
+          if(err) console.log('oh no!');
+          
+          assert.equal(doc.items.length, 100);
+          
+          for(var j = 0, k = 100; j < k; j++){
+            assert.equal(doc.items.get(j), j);
+          }
+          
+          console.log('it worked!');
+          mongoose.disconnect();
+          
+        });
+      }
+    })
+  }
+   
+   
